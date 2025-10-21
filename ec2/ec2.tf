6c6c6d5ac5546eb619f40ab76b09fd4d6fbeee89
gweanqu0 <gweanquoi9@gmail.com>
2025-10-21 13:38:51 -0400

    mudule

---
+++ ec2/ec2.tf
@@ -0,0 +1,8 @@
+module "ec2-server" {
+    source = "../"
+    ami = "ami-0331ebbf81138e4de"
+    region_name = "us-east-2"
+    profile_name = "default"
+    instance_type = "t3.micro"
+}
+
--- terraform.tfvars
+++
@@ -1,4 +0,0 @@
-ami .=. "ami-090e0fc566929d98b"
-region_name .=. "us-east-1"
-profile_name .=. "default"
-instance_type .=. "t2.micro"
